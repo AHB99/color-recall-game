2331aa8c984fc25385da708fbd6acb82d4cdfc40
Azadan Bhagwagar <azadanhb99@gmail.com>
2020-01-03 22:11:21 +0400

    Added basic Remember screen
    Added components and styles for rough sketch of Remember screen.

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -1,6 +1,6 @@
 'use strict'
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
@@ -12,9 +12,45 @@ export default class MainScreen extends React.Component{
 
     render(){
         return (
-        <View>
-            <Text>Hii World!</Text>
+        <View style={styles.container}>
+            <Text style={styles.rememberText}>Remember this color!</Text>
+            <Text style={styles.timerText}>3 seconds left</Text>
+            <View style={styles.rectangle} />
         </View>
         );
     }
 }
+
+///Styles---------------
+
+const elem = {
+    padding: 20
+}
+
+
+let styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'white'
+        },
+        rectangle: {
+            width: '80%',
+            height: '40%',
+            backgroundColor: '#ff0000',
+            borderRadius: 20,
+            ...elem,
+        },
+        rememberText: {
+            fontFamily: 'serif',
+            fontSize: 30,
+            ...elem,
+        },   
+        timerText: {
+            fontFamily: 'serif',
+            fontSize: 20,
+            ...elem,
+        }    
+    }
+)
