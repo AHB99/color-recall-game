d150c7840ea04f98ad3c09aa8e149794e03ad9d4
Azadan Bhagwagar <azadanhb99@gmail.com>
2020-01-22 21:11:48 +0400

    Added UI for list of high scores

--- src/HighScoreComponent.js
+++ src/HighScoreComponent.js
@@ -1,17 +1,16 @@
 'use strict'
 import React from 'react';
 import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, TouchableHighlight,
-TouchableWithoutFeedback } from 'react-native';
+TouchableWithoutFeedback, SectionList } from 'react-native';
 
 import * as ColorGenerationFunctions from './ColorGenerationFunctions';
 import { MainGameConstants, RgbColorBundle, GameMode } from './GameUtils';
 import * as GameUtils from './GameUtils';
-import RememberComponent from './RememberComponent'; 
-import RewardComponent from './RewardComponent'; 
-import RecallComponent from './RecallComponent'; 
 import * as GameStyles from './GameStyles';
 import ButtonComponent from './ButtonComponent';
 
+//DEBUGGING
+const testData = [{title: 1, data: [76, 54, 20]}];
 
 /**
  * Component to display screen for high scores.
@@ -28,10 +27,8 @@ export default class HighScoreComponent extends React.Component{
         super(props);
         this.state = {
             selectedGameMode: GameMode.ACCURACY,
-
         }
     }
-
 
     render(){
         return (
@@ -43,12 +40,39 @@ export default class HighScoreComponent extends React.Component{
                     <TabComponent text={'Speed'} tabSymbol={GameMode.SPEED} selectedSymbol={this.state.selectedGameMode}
                     onTabPressed={this._onGameModeTabSelected}></TabComponent>
                 </View>
+                <View style={styles.listTableHeaderContainer}>
+                    <View style={styles.listItemCellContainer}>
+                        <Text style={styles.listTableHeaderTextStyle}>Rank</Text>
+                    </View>
+                    <View style={styles.listItemCellContainer}>
+                        <Text style={styles.listTableHeaderTextStyle}>Score</Text>
+                    </View>
+                </View>
+                <View style={styles.listContainer}>
+                    <SectionList 
+                    sections={testData}
+                    keyExtractor={(item, index) => index}
+                    renderItem={this._renderHighScoreItem}
+                    renderSectionHeader={this._renderHighScoreDifficultyHeader}/>
+                </View>
             </SafeAreaView>
         );
     }
 
     _onGameModeTabSelected = (gameMode) => {
         this.setState({selectedGameMode: gameMode});
+    }
+
+    _renderHighScoreItem = ({item, index}) => {
+        return (<HighScoreItemComponent rank={index+1} score={item}/>);
+    }
+
+    _renderHighScoreDifficultyHeader = ({ section: { title } }) => {
+        return (
+            <View style={styles.listSectionHeaderContainer}>
+                <Text style={styles.listSectionHeaderTextStyle}>Difficulty {title}</Text>
+            </View>
+        );
     }
 }
 
@@ -73,8 +97,43 @@ class TabComponent extends React.Component {
 
 }
 
+/**
+ * Component representing a single high scores row.
+ * @function
+ * 
+ * @param {number} props.rank
+ * @param {number} props.score
+ */
+function HighScoreItemComponent(props){
+    return (
+        <View style={styles.listItemContainer}>
+            <View style={styles.listItemCellContainer}>
+                <Text style={styles.listItemCellTextStyle}>#{props.rank}</Text>
+            </View>
+            <View style={styles.listItemCellContainer}>
+                <Text style={styles.listItemCellTextStyle}>{props.score}%</Text>
+            </View>
+        </View>
+    );
+}
+
+
+
+//Styles
 const selectedTabColor = '#2196F3';
 const deselectedTabColor = '#C8E5FC';
+
+const listItemCellTextStyle = {
+    fontSize: 25,
+    fontFamily: 'serif',
+    textAlign: 'center',
+}
+
+const listItemContainer = {
+    flexDirection: 'row',
+    alignSelf: 'stretch',
+}
+
 
 let styles = StyleSheet.create({
     bodyContainer: {
@@ -87,7 +146,7 @@ let styles = StyleSheet.create({
         fontFamily: 'serif',
         fontSize: 50,
         textAlign: 'center',
-        margin: 20,
+        margin: 25,
     },
     tabsContainer: {
         ...GameStyles.footerContainer,
@@ -102,6 +161,38 @@ let styles = StyleSheet.create({
         fontFamily: 'monospace',
         color: 'white',
         fontWeight: 'bold'
-    }
+    },
+    listContainer: {
+        justifyContent: 'flex-start',
+        alignItems: 'stretch',
+        flex: 1,
+    },
+    listItemContainer: listItemContainer,
+    listItemCellContainer: {
+        flex: 1,
+        alignSelf: 'stretch',
+        padding: 5,
+    },
+    listItemCellTextStyle: listItemCellTextStyle,
+    listTableHeaderContainer: {
+        ...listItemContainer,
+        borderTopWidth: 0.5,
+        borderBottomWidth: 0.5,
+
+    },
+    listTableHeaderTextStyle: {
+        ...listItemCellTextStyle,
+        fontWeight: 'bold',
+    },
+    listSectionHeaderContainer: {
+        alignSelf: 'stretch',
+        backgroundColor: '#FAFAFA',
+    },
+    listSectionHeaderTextStyle: {
+        fontSize: 25,
+        fontFamily: 'sans-serif-thin',
+        textAlign: 'center',
+        fontWeight: '600'
+    },
 
 });
